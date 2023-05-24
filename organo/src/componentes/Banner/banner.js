c9439fb9cb98ecf019501b6f926c726826c3cf49
James Holanda <jameshsousa@gmail.com>
2023-05-24 00:45:34 -0300

    adding banner

--- organo/src/componentes/Banner/banner.js
+++ organo/src/componentes/Banner/banner.js
@@ -1,4 +1,13 @@
+import "./banner.css"
+
 function Banner() {
+    return (
+        //JSX
+        <header className="banner">
+            <img src="/imagens/banner.png" alt="O banner principal da página do Organo" />
+        </header>
+        
+    )
 
 }
 
