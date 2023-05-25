a7dcb5457b21895ca26a60dba6a6b1ea47d1212b
James Holanda <jameshsousa@gmail.com>
2023-05-24 23:24:40 -0300

    Adicionando camponente campoTexto

---
+++ organo/src/componentes/campoTexto/index.js
@@ -0,0 +1,14 @@
+import './campoTexto.css'
+
+const CampoTexto = () => {
+
+    return (
+        <div className="campo-texto">
+            <label>Nome</label>
+            <input placeholder='Digite seu nome' />
+        </div>
+    )
+
+}
+
+export default CampoTexto
