2f768b18044e262b8d62511c3114bb3e510cd764
James Holanda <jameshsousa@gmail.com>
2023-05-26 00:24:56 -0300

    adicionando o componente ListaSuspensa

--- organo/src/componentes/Formulario/index.js
+++ organo/src/componentes/Formulario/index.js
@@ -1,7 +1,19 @@
+import ListaSuspensa from '../ListaSuspensa'
 import CampoTexto from '../campoTexto'
 import './Formulario.css'
 
 const Formulario = () => {
+    const times = [
+        'Programação',
+        'Front-End',
+        'Data Science',
+        'Data Science',
+        'Devops',
+        'UX e Design',
+        'Mobile',
+        'Inovação e Gestão'
+    ]
+    
     return (
         <section className="formulario">
             <form>
@@ -9,6 +21,7 @@ const Formulario = () => {
             <CampoTexto label="Nome" placeholder="Digite seu nome" />
             <CampoTexto label="Cargo" placeholder="Digite seu cargo" />
             <CampoTexto label="Imagem" placeholder="Digite o endeço da imagem" />
+            <ListaSuspensa label="Time" itens = {times} />
 
             </form>
         </section>
