f6e459dbe3553c30f6036a61239d919ca228c837
James Holanda <jameshsousa@gmail.com>
2023-05-27 19:28:23 -0300

    Criando função onChange no input

--- organo/src/componentes/campoTexto/index.js
+++ organo/src/componentes/campoTexto/index.js
@@ -2,10 +2,18 @@ import './campoTexto.css'
 
 const CampoTexto = (props) => {
 
+    let valor = '';
+
+    
+    const aoDigitado = (evento) => {
+        valor = evento.target.value;
+        console.log(valor)
+    }
+
     return (
         <div className="campo-texto">
             <label>{props.label}</label>
-            <input required={props.obrigatorio} placeholder= {props.placeholder} />
+            <input value={valor} onChange={aoDigitado} required={props.obrigatorio} placeholder= {props.placeholder} />
         </div>
     )
 
